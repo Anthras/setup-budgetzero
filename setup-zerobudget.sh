196580242fc735c6edaab9dbb16dd94203c194f3
Anthras <88218711+Anthras@users.noreply.github.com>
2022-05-07 11:08:25 -0500

    Add files via upload
    Add shell script

---
+++ setup-zerobudget.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo apt -y update && sudo apt -y upgrade
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get -y update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo docker pull budgetzero/budgetzero:latest
+sudo docker run -d -p 8080:8080 --name budgetzero budgetzero/budgetzero
+sudo docker run -d -p 5984:5984 --name budgetzero-couchdb-sync -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=password couchdb:latest
+echo "see https://github.com/budgetzero/budgetzero for remainder of couchdb setup"
